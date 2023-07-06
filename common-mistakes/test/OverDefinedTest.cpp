e3e368ae020b598a24856ddd3c3b6a9bb6a57aff
Simon Bader <simon.bader@scs.ch>
2023-07-06 13:55:21 +0200

    Add over defined statements.

--- common-mistakes/test/OverDefinedTest.cpp
+++ common-mistakes/test/OverDefinedTest.cpp
@@ -20,9 +20,9 @@ class ProcessorMock : public Processor {
 TEST(OverDefinedTest, SumUp) {
   // arrange
   std::shared_ptr<ProducerMock> producer = std::make_shared<ProducerMock>();
-  EXPECT_CALL(*producer, Create()).WillOnce(Return(std::vector<int>{10, 20, 30}));
+  EXPECT_CALL(*producer, Create()).Times(1).WillOnce(Return(std::vector<int>{10, 20, 30}));
   std::shared_ptr<ProcessorMock> processor = std::make_shared<ProcessorMock>();
-  EXPECT_CALL(*processor, SumUp(std::vector<int>{10, 20, 30})).WillOnce(Return(60));
+  EXPECT_CALL(*processor, SumUp(std::vector<int>{10, 20, 30})).Times(1).WillOnce(Return(60));
   OverDefined sut(producer, processor);
 
   // act
